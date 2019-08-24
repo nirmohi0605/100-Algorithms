0c6a9669334d93d53e0c94a584c5619358c5c6fd
Nirmohi <nirmohi0605@gmail.com>
2019-08-24 12:22:54 -0400

    19

---
+++ arrayReplace/arrayReplace.ts
@@ -0,0 +1,12 @@
+function arrayReplace(inputArray: number[], elemToReplace: number, substitutionElem: number): number[] {
+   
+    for(let i = 0; i < inputArray.length; i++) {
+        if(inputArray[i] === elemToReplace) {
+            inputArray[i] = substitutionElem;
+        }
+    }
+
+    return inputArray;
+}
+
+console.log(arrayReplace([1, 2, 1], 1, 3));
