c3a2c14787fbfe30dfd68990af8ea473f427e77e
Nirmohi <nirmohi0605@gmail.com>
2019-08-24 17:10:17 -0400

    19

--- avoidObstacles/avoidObstacles.ts
+++ avoidObstacles/avoidObstacles.ts
@@ -1,5 +1,15 @@
 function avoidObstacles(inputArray: number[]): number {
 
+    inputArray = inputArray.sort((a,b) => a-b);
+    let maxElement = Math.max(...inputArray);
+
+    for(let i = 1; i < maxElement + 1; i++) {
+        let flag = inputArray.every((el) => {
+            return (el % i !== 0)
+        })
+        if(flag) return i;
+    }
+    return maxElement + 1;
 }
 
-console.log(avoidObstacles([5, 3, 6, 7, 9]));
+console.log(avoidObstacles([1,3,4,5]));
