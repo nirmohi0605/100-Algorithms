3c9e93e9ae30ec095e2e959918fe0f38578901a1
Nirmohi <nirmohi0605@gmail.com>
2019-08-26 17:54:30 -0400

    20

--- bishopAndPawn/bishopAndPawn.ts
+++ bishopAndPawn/bishopAndPawn.ts
@@ -1,5 +1,10 @@
 function bishopAndPawn(bishop: string, pawn: string): boolean {
+    let bishopCharCode = bishop.charAt(0).charCodeAt(0);
+    let pawnCharCode = pawn.charAt(0).charCodeAt(0);
 
+    let columnDifference = Math.abs(parseInt(bishop.charAt(1)) - parseInt(pawn.charAt(1)));
+
+    return Math.abs(bishopCharCode - pawnCharCode) === columnDifference
 }
 
-console.log(bishopAndPawn('a1', 'c3'));
+console.log(bishopAndPawn('a6', 'a6'));
